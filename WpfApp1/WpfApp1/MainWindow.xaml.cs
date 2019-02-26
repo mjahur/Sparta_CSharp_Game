17a22ef9ec8c7fd8dfc6aee264de63ddc758e877
Mishu <mjahur@spartaglobal.com>
2019-02-26 23:19:12 +0000

    Construct via Array

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UserInput;
+using Microsoft.VisualBasic;
 
 namespace WpfApp1
 {
@@ -20,16 +13,99 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        NumberAssignment NA = new NumberAssignment();
+        User_Input ui = new User_Input();
         public MainWindow()
         {
             InitializeComponent();
-            }
-        
-        private void B1_TextChanged_1(object sender, TextChangedEventArgs e)
+            BuildSudokuGrid();
+
+
+        }
+
+        int[,] SudokuArray = new int[9, 9]
         {
-            B1.Text = "5";
+            {5,0,0,1,0,0,0,0,0},
+            {0,9,6,0,0,0,8,2,0},
+            {0,0,0,0,0,7,0,0,9},
+            {0,0,0,0,0,3,0,0,6},
+            {0,7,4,0,0,0,9,1,0},
+            {2,0,0,5,0,0,0,0,0},
+            {7,0,0,6,0,0,0,0,0},
+            {0,8,3,0,0,0,5,7,0},
+            {0,0,0,0,0,4,0,0,1},
+        };
+
+        public Button getGridChild(int r, int c)
+        {
+            for (int i = 0; i < SudokuGrid.Children.Count; i++)
+            {
+                Button e = (Button) SudokuGrid.Children[i];
+                if (Grid.GetRow(e) == r && Grid.GetColumn(e) == c)
+                    return e;
+            }
+            return null;
+        }
+
+        public void BuildSudokuGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                     if (SudokuArray[i,j] != 0)
+                    {
+                        getGridChild(i, j).Content = SudokuArray[i, j];
+                    }
+                }
+            }
+        }
+
+        public void SubGridValidity()
+        {
+            foreach (var item in this.SudokuGrid.Children)
+            {
+                Grid subgrid = (Grid)item;
+                for (int i = 0; i < subgrid.Children.Count; i++)
+                {
+                    TextBox e = (TextBox)subgrid.Children[i];
+                    for (int j = 0; j < subgrid.Children.Count; j++)
+                    {
+                        TextBox f = (TextBox)subgrid.Children[j];
+
+                        if (f != e && f.Text == e.Text)
+                        {
+                            e.Text = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void UserInput()
+        {
+            Interaction.InputBox("Select ", "Title", "Default Text");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+            var button = (Button)sender;
+
+            for (int i = 1; i < 10; i++)
+            {
+                numbers.Add(i);
+            }
+                string x = Interaction.InputBox("Choice", "Enter choice", null);
+            if(x == "1" || x == "2" || x == "3" || x == "4" || x == "5" || x == "6" || x == "7" || x == "8" || x == "9")
+            {
+                button. = Brushes.Blue;
+                button.Content = Int32.Parse(x);
+            }
+            else
+            {
+                MessageBox.Show("You must enter a number between 1 and 9");
+            }
         }
     }
    
-    }
+}
--- WpfApp1/WpfApp1/NumberAssignment.cs
+++ WpfApp1/WpfApp1/NumberAssignment.cs
@@ -6,28 +6,85 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace WpfApp1
+
+namespace UserInput
 {
-    class NumberAssignment
+    class User_Input
     {
 
-        List<int> perm1;
-        List<int> perm2;
-        List<int> perm3;
-        List<int> perm4;
-
-        public List<int> Perm1 { get => perm1; set => perm1 = value; }
-        public List<int> Perm2 { get => perm2; set => perm2 = value; }
-        public List<int> Perm3 { get => perm3; set => perm3 = value; }
-        public List<int> Perm4 { get => perm4; set => perm4 = value; }
-
-        public NumberAssignment()
+        public UIElement subGrid(Grid g, int i)
         {
+            return g.Children[i];
         }
 
-        public void textChangedEventHandler(object sender, TextChangedEventArgs args)
+        public void Validity(Grid g , int r, int c)
         {
-            
+            for (int i = 0; i < g.Children.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Grid SubInt0 = (Grid)g.Children[i];
+
+                }
+                else if(i == 1)
+                {
+                    Grid SubInt1 = (Grid)g.Children[i];
+                }
+                else if (i == 2)
+                {
+                    Grid SubInt2 = (Grid)g.Children[i];
+                }
+                else if (i == 3)
+                {
+                    Grid SubInt3 = (Grid)g.Children[i];
+                }
+                else if (i == 4)
+                {
+                    Grid SubInt4 = (Grid)g.Children[i];
+                }
+                else if (i == 5)
+                {
+                    Grid SubInt5 = (Grid)g.Children[i];
+                }
+                else if (i == 6)
+                {
+                    Grid SubInt6 = (Grid)g.Children[i];
+                }
+                else if (i == 7)
+                {
+                    Grid SubInt7 = (Grid)g.Children[i];
+                }
+                else if (i == 8)
+                {
+                    Grid SubInt8 = (Grid)g.Children[i];
+                }
+            }
+           
+            /*for (int i = 0; i < g.Children.Count; i++)
+            {
+
+            }
+           /* for (int i = 0; i < g.Children.Count; i++)
+            {
+                TextBox e = (TextBox) g.Children[i];
+                if (Grid.GetRow(e) == r && Grid.GetColumn(e) == c)
+                {
+                    for (int j = 0; j < g.Children.Count; j++)
+                    {
+                        TextBox f = (TextBox)g.Children[j];
+
+                        if (((Grid.GetRow(f)==Grid.GetRow(e)) && (Grid.GetColumn(f)!= Grid.GetColumn(e)) && f.Text == e.Text) 
+                            || ((Grid.GetRow(f) != Grid.GetRow(e)) && (Grid.GetColumn(f) == Grid.GetColumn(e)) && (f.Text == e.Text) && e.Text != null))
+                        {
+                            MessageBox.Show("Repeat in a row or column");
+                            e.Text = null;
+                        }
+                    }
+                }
+
+            }*/
+
         }
+        
     }
 }
